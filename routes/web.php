bc0a27586bdd4f117a7b97991ae5ed297130af33
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-06-15 10:53:57 +0700

    commit from YOEUNG YENG

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
+Route::view('article', 'article')->name('article');
